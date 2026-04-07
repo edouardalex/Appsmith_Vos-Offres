b4ae4e3e8e5ee29c8d548e885f203628159b8ee3
quentin <quentin@ideel.io>
2026-04-07 16:10:37 +0000

    Amélioré forfait mobile et internet

---
+++ pages/Forfait mobile Copy/jsobjects/Constants/Constants.js
@@ -0,0 +1,16 @@
+export default {
+	colors: {
+		font: "#596d7b",
+		success: "#5CBF86",
+		failure: "#E6007E",
+	},
+
+	operatorsToHideMap: {
+		"sfr": ["sfr", "red_by_sfr"],
+		"red_by_sfr": ["sfr", "red_by_sfr"],
+		"sosh": ["sosh", "orange"],
+		"orange": ["sosh", "orange"],
+		"free": ["free"],
+		"bouygues_telecom": ["bouygues_telecom"]
+	},
+};
